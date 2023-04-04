b113469fb766635aa6b10767555b7dbf1fc444ec
“LlanesCarlos” <cllanes.llanes@gmail.com>
2023-04-04 01:26:15 -0400

    Ejemplo de mini proyecto.

---
+++ src/components/emojiPicker/data.js
@@ -0,0 +1,22 @@
+export const data = [
+    {
+        symbol: "😀",
+        name: "cara feliz",
+        keywords: "smile happy smiling",
+    },
+    {
+        symbol: "❤",
+        name: "corazón",
+        keywords: "heart love like",
+    },
+    {
+        symbol: "😠",
+        name: "cara enojada",
+        keywords: "angry rage fury furious raging",
+    },
+    {
+        symbol: "🤑",
+        name: "cara dinero",
+        keywords: "money",
+    },
+];
---
+++ src/components/emojiPicker/emojiPicker.js
@@ -0,0 +1,9 @@
+import { forwardRef } from "react";
+
+export function EmojiPicker(props, inputRef){
+    return <div>
+        <button>😀video en 1:47:20</button>
+    </div>;
+}
+
+export default forwardRef(EmojiPicker);
